5daec015e13ee4891a5f0603892d9c0638ec4536
lucas-da-silva <lucassouza00012345@gmail.com>
2022-11-30 19:49:26 -0300

    feat: create model Category

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      underscored: true,
+      tableName: 'categories',
+      timestamps: false
+    }
+  );
+  return Category;
+};

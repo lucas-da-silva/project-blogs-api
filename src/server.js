9977d311239cccbbe00249641ea47f1c560154d1
lucas-da-silva <lucassouza00012345@gmail.com>
2022-12-02 09:00:52 -0300

    refactor: remove unnecessary comments

--- src/server.js
+++ src/server.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 const app = require('./app');
 
-// não remova a variável `API_PORT` ou o `listen`
 const port = process.env.API_PORT || 3000;
-
-// não remova esse endpoint
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-app.listen(port, () => console.log('ouvindo porta', port));
+app.listen(port, () => console.log('listening door', port));
